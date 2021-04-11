1f0803c5e0646a8f0d26e54252ffa9766c21b71d
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2021-04-11 18:38:13 -0400

    updated info

--- src/main/java/lol/hyper/servercore/tools/AutoMessages.java
+++ src/main/java/lol/hyper/servercore/tools/AutoMessages.java
@@ -11,7 +11,7 @@ public class AutoMessages {
     public AutoMessages() {
         messages.put(1, "Make sure to /vote! Voting gives you a blue name.");
         messages.put(2, "Hey you! Make sure to vote. Each vote gives you 2 dupe charges. That means you can /dupe twice!");
-        messages.put(3, "Hey monkeys, check out the subreddit!");
+        messages.put(3, "Hey retards, check out the subreddit!");
         messages.put(4, "This map will reset on June 12th.");
     }
 
