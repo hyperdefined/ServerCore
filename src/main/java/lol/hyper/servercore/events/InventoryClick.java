50f7274c7b1140c5610d78ec87c86a9faa7a9dd3
hyperdefined <contact@hyper.lol>
2021-06-29 10:55:41 -0400

    much improved anvil renaming

--- src/main/java/lol/hyper/servercore/events/InventoryClick.java
+++ src/main/java/lol/hyper/servercore/events/InventoryClick.java
@@ -1,12 +1,12 @@
 package lol.hyper.servercore.events;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -15,24 +15,19 @@ public class InventoryClick implements Listener {
     // this let's players have colors in names, kinda shit but it works
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onInventoryClick(InventoryClickEvent event) {
+        if (event.isCancelled() || event.getCurrentItem() == null) {
+            return;
+        }
+
         Inventory inventory = event.getInventory();
         if (inventory instanceof AnvilInventory) {
-            InventoryView view = event.getView();
-            if (event.getRawSlot() == view.convertSlot(event.getRawSlot())) {
-                if (event.getRawSlot() == 2) {
-                    ItemStack item = event.getCurrentItem();
-                    if (item != null) {
-                        ItemMeta meta = item.getItemMeta();
-                        if (meta != null) {
-                            if (meta.hasDisplayName()) {
-                                String displayName = meta.getDisplayName();
-                                meta.setDisplayName(
-                                        net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', displayName));
-                                item.setItemMeta(meta);
-                            }
-                        }
-                    }
-                }
+            AnvilInventory inv = (AnvilInventory) event.getInventory();
+            ItemStack itemStack = inv.getItem(2);
+            ItemMeta meta = itemStack.getItemMeta();
+            if (meta.hasDisplayName()) {
+                String displayName = meta.getDisplayName();
+                meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', displayName));
+                itemStack.setItemMeta(meta);
             }
         }
     }
