ed073068c3f88fa64d6226e509d990089b35966c
hyperdefined <contact@hyper.lol>
2021-06-30 02:39:04 -0400

    r

--- src/main/java/lol/hyper/servercore/commands/CommandRules.java
+++ src/main/java/lol/hyper/servercore/commands/CommandRules.java
@@ -12,8 +12,9 @@ public class CommandRules implements CommandExecutor {
     public boolean onCommand(CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
         sender.sendMessage(
-                ChatColor.DARK_AQUA + "Don't break U.S. law. Using hack/cheat clients is allowed and encouraged.");
-        sender.sendMessage(ChatColor.DARK_AQUA + "We block game breaking exploits like flight.");
+                ChatColor.DARK_AQUA
+                        + "The server blocks game breaking “exploits,” such as extreme speeds and flight. Players are able to do anything they want, just don’t be dumb. It’s a block game.");
+        sender.sendMessage(ChatColor.DARK_AQUA + "Any extreme cases could lead into a ban from the server.");
         sender.sendMessage(ChatColor.GOLD + "--------------------------------------------");
         return true;
     }
